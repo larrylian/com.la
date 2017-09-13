1673cfb49ef5021264fb92ef1bdae4b83487b388
Larry <554538252@qq.com>
2017-09-14 01:45:06 +0800

    完成页面设计

--- lamp/src/app/app.component.ts
+++ lamp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Larry's lamp Controller Demo";
+  title = "Larry's lamp Demo";
 }
