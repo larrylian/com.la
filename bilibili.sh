9b3494039894005de8158cef90fe15e0ba86464a
Larry <554538252@qq.com>
2017-09-14 11:20:09 +0800

    bilibli

--- bilibili.sh
+++ bilibili.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-int = 0
-max = 3
+int=0
+max=3
 while [$int -lt $max]
 do
 sudo echo 1 > /sys/class/gpio/gpio18/value
 sleep 1;
 sudo echo 0 > /sys/class/gpio/gpio18/value
 sleep 1;
-int++
+let "int++"
 done
